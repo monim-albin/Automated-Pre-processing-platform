7abee4b9365db17ba08486630bd683a7efd6a528
monim <61268102+monim-albin@users.noreply.github.com>
2022-05-24 09:30:08 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,3 +1,4 @@
+import base64
 import emoji
 import arabicstopwords.arabicstopwords as stp
 from tashaphyne.stemming import ArabicLightStemmer
@@ -9,34 +10,44 @@ import streamlit as st
 from io import StringIO
 
 
+maxUploadSize=10
+
 class Preprocessing:
     def __init__(self):
     # variables
         self.df = None
         self.ff = None
         self.emoji_loca = None
+        self.dataset_try = None
         self.col_selected = None
         self.stop_word_comp = {"،","آض","آمينَ","آه","آهاً","آي","أ","أب","أجل","أجمع","أخ","أخذ","أصبح","أضحى","أقبل","أقل","أكثر","ألا","أم","أما","أمامك","أمامكَ","أمسى","أمّا","أن","أنا","أنت","أنتم","أنتما","أنتن","أنتِ","أنشأ","أنّى","أو","أوشك","أولئك","أولئكم","أولاء","أولالك","أوّهْ","أي","أيا","أين","أينما","أيّ","أَنَّ","أََيُّ","أُفٍّ","إذ","إذا","إذاً","إذما","إذن","إلى","إليكم","إليكما","إليكنّ","إليكَ","إلَيْكَ","إلّا","إمّا","إن","إنّما","إي","إياك","إياكم","إياكما","إياكن","إيانا","إياه","إياها","إياهم","إياهما","إياهن","إياي","إيهٍ","إِنَّ","ا","ابتدأ","اثر","اجل","احد","اخرى","اخلولق","اذا","اربعة","ارتدّ","استحال","اطار","اعادة","اعلنت","اف","اكثر","اكد","الألاء","الألى","الا","الاخيرة","الان","الاول","الاولى","التى","التي","الثاني","الثانية","الذاتي","الذى","الذي","الذين","السابق","الف","اللائي","اللاتي","اللتان","اللتيا","اللتين","اللذان","اللذين","اللواتي","الماضي","المقبل","الوقت","الى","اليوم","اما","امام","امس","ان","انبرى","انقلب","انه","انها","او","اول","اي","ايار","ايام","ايضا","ب","بات","باسم","بان","بخٍ","برس","بسبب","بسّ","بشكل","بضع","بطآن","بعد","بعض","بك","بكم","بكما","بكن","بل","بلى","بما","بماذا","بمن","بن","بنا","به","بها","بي","بيد","بين","بَسْ","بَلْهَ","بِئْسَ","تانِ","تانِك","تبدّل","تجاه","تحوّل","تلقاء","تلك","تلكم","تلكما","تم","تينك","تَيْنِ","تِه","تِي","ثلاثة","ثم","ثمّ","ثمّة","ثُمَّ","جعل","جلل","جميع","جير","حار","حاشا","حاليا","حاي","حتى","حرى","حسب","حم","حوالى","حول","حيث","حيثما","حين","حيَّ","حَبَّذَا","حَتَّى","حَذارِ","خلا","خلال","دون","دونك","ذا","ذات","ذاك","ذانك","ذانِ","ذلك","ذلكم","ذلكما","ذلكن","ذو","ذوا","ذواتا","ذواتي","ذيت","ذينك","ذَيْنِ","ذِه","ذِي","راح","رجع","رويدك","ريث","رُبَّ","زيارة","سبحان","سرعان","سنة","سنوات","سوف","سوى","سَاءَ","سَاءَمَا","شبه","شخصا","شرع","شَتَّانَ","صار","صباح","صفر","صهٍ","صهْ","ضد","ضمن","طاق","طالما","طفق","طَق","ظلّ","عاد","عام","عاما","عامة","عدا","عدة","عدد","عدم","عسى","عشر","عشرة","علق","على","عليك","عليه","عليها","علًّ","عن","عند","عندما","عوض","عين","عَدَسْ","عَمَّا","غدا","غير","ـ","ف","فان","فلان","فو","فى","في","فيم","فيما","فيه","فيها","قال","قام","قبل","قد","قطّ","قلما","قوة","كأنّما","كأين","كأيّ","كأيّن","كاد","كان","كانت","كذا","كذلك","كرب","كل","كلا","كلاهما","كلتا","كلم","كليكما","كليهما","كلّما","كلَّا","كم","كما","كي","كيت","كيف","كيفما","كَأَنَّ","كِخ","لئن","لا","لات","لاسيما","لدن","لدى","لعمر","لقاء","لك","لكم","لكما","لكن","لكنَّما","لكي","لكيلا","للامم","لم","لما","لمّا","لن","لنا","له","لها","لو","لوكالة","لولا","لوما","لي","لَسْتَ","لَسْتُ","لَسْتُم","لَسْتُمَا","لَسْتُنَّ","لَسْتِ","لَسْنَ","لَعَلَّ","لَكِنَّ","لَيْتَ","لَيْسَ","لَيْسَا","لَيْسَتَا","لَيْسَتْ","لَيْسُوا","لَِسْنَا","ما","ماانفك","مابرح","مادام","ماذا","مازال","مافتئ","مايو","متى","مثل","مذ","مساء","مع","معاذ","مقابل","مكانكم","مكانكما","مكانكنّ","مكانَك","مليار","مليون","مما","ممن","من","منذ","منها","مه","مهما","مَنْ","مِن","نحن","نحو","نعم","نفس","نفسه","نهاية","نَخْ","نِعِمّا","نِعْمَ","ها","هاؤم","هاكَ","هاهنا","هبّ","هذا","هذه","هكذا","هل","هلمَّ","هلّا","هم","هما","هن","هنا","هناك","هنالك","هو","هي","هيا","هيت","هيّا","هَؤلاء","هَاتانِ","هَاتَيْنِ","هَاتِه","هَاتِي","هَجْ","هَذا","هَذانِ","هَذَيْنِ","هَذِه","هَذِي","هَيْهَاتَ","و","و6","وا","واحد","واضاف","واضافت","واكد","وان","واهاً","واوضح","وراءَك","وفي","وقال","وقالت","وقد","وقف","وكان","وكانت","ولا","ولم","ومن","مَن","وهو","وهي","ويكأنّ","وَيْ","وُشْكَانََ","يكون","يمكن","يوم","ّأيّان"}
         self.ArListem = ArabicLightStemmer()
         self.emojis_ar = {}
-        self.loves = ["<3", "♥", '❤']
-        self.smilefaces = []
-        self.sadfaces = []
-        self.neutralfaces = []
-        self.eyes = ["8", ":", "=", ";"]
-        self.nose = ["'", "`", "-", r"\\"]
-        self.define_emotional_emoji()
+        self.emoji_pattern = re.compile("["
+                                   u"\U0001F600-\U0001F64F"  # emoticons
+                                   u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                                   u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                                   u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                                   u"\U00002702-\U000027B0"
+                                   u"\U000024C2-\U0001F251"
+                                   "]+", flags=re.UNICODE)
 
-    # @st.cache
+    def download(self, dff, text_to_view, type, location, save_type):
+        if save_type == "Text":
+            csv = dff.to_csv(index=False, sep='\t')
+        else:
+            csv = dff.to_csv(index=False, sep=',')
+
+        b64 = base64.b64encode(csv.encode()).decode()
+        href = f'<a href="data:{type};base64,{b64}" download="{location}">{text_to_view}</a>'
+        return href
+
     def read_file(self, file):
-        print(file)
         sep = "\t" if file.type == "text/plain" else ";"
         bytes_data = file.read()
-
         try:
             encoding = 'UTF-16'
             s = str(bytes_data, encoding)
-
         except:
             encoding = 'UTF-8'
             s = str(bytes_data, encoding)
@@ -44,6 +55,12 @@ class Preprocessing:
         data = StringIO(s)
         s = pd.read_csv(data, delimiter=sep)
         self.df = s
+
+    @st.cache
+    def read_file_return(self, file):
+        s = pd.read_csv(file, delimiter="\t", encoding="UTF-8")
+        return s
+
 
     # <--------- Adding new features ------------>
     # return string columns
@@ -103,134 +120,42 @@ class Preprocessing:
                              """, re.VERBOSE)
         text = re.sub(noise, '', text)
         text = re.sub(re.compile(r'(.)\1+'), r"\1\1", text)  # Remove longation
-        return araby.strip_tashkeel(text)
+        return araby.strip_diacritics(text)
 
 
     def remove_stop_words(self, text):
         zen = TextBlob(text)
-        # words = zen.words()
         words = zen.split()
         return " ".join([w for w in words if not stp.is_stop(w) and not w in self.stop_word_comp and len(w) >= 2])
 
     # Read all emojis from files
-    @st.cache
+    # @st.cache
     def emoji_list(self):
-        # with open('./emojis.csv', 'r', encoding='utf-8') as f:
         with open(self.emoji_loca, 'r', encoding='utf-8') as f:
             lines = f.readlines()
             for line in lines:
                 line = line.strip('\n').split(';')
-                self.emojis_ar.update({line[0].strip(): line[1].strip()})
+                self.emojis_ar[line[0].strip()]= line[1].strip()
 
     def remove_emoji(self, text):
-        emoji_pattern = re.compile("["
-                                   u"\U0001F600-\U0001F64F"  # emoticons
-                                   u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-                                   u"\U0001F680-\U0001F6FF"  # transport & map symbols
-                                   u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-                                   u"\U00002702-\U000027B0"
-                                   u"\U000024C2-\U0001F251"
-                                   "]+", flags=re.UNICODE)
-        text = emoji_pattern.sub(r'', text)
+        text = self.emoji_pattern.sub(r'', text)
         return text
 
-    # @st.cache
-    def define_emotional_emoji(self):
-
-        for e in self.eyes:
-            for n in self.nose:
-                for s in ["\)", "d", "]", "}", "p"]:
-                    self.smilefaces.append(e + n + s)
-                    self.smilefaces.append(e + s)
-                for s in ["\(", "\[", "{"]:
-                    self.sadfaces.append(e + n + s)
-                    self.sadfaces.append(e + s)
-                for s in ["\|", "\/", r"\\"]:
-                    self.neutralfaces.append(e + n + s)
-                    self.neutralfaces.append(e + s)
-                # reversed
-                for s in ["\(", "\[", "{"]:
-                    self.smilefaces.append(s + n + e)
-                    self.smilefaces.append(s + e)
-                for s in ["\)", "\]", "}"]:
-                    self.sadfaces.append(s + n + e)
-                    self.sadfaces.append(s + e)
-                for s in ["\|", "\/", r"\\"]:
-                    self.neutralfaces.append(s + n + e)
-                    self.neutralfaces.append(s + e)
-        self.smilefaces = list(set(self.smilefaces))
-        self.sadfaces = list(set(self.sadfaces))
-        self.neutralfaces = list(set(self.neutralfaces))
-
-    def emoji_native_translation(self, text):
-        text = re.sub(r'\d+(.*?)(?:\u263a|\U0001f645)', self.cehck_emoji, text)
-        return text
-
-    def cehck_emoji(self, word):
-        word = word.group()
-        if word in self.loves:
-            return "حب"
-        elif word in self.smilefaces:
-            return "مضحك"
-        elif word in self.neutralfaces:
-            return "عادي"
-        elif word in self.sadfaces:
-            return "محزن"
-        else:
-            return word
-
-    def is_emoji(self, word):
-        if word in self.emojis_ar:
-            return True
-        else:
-            return False
-
-    def add_space(self, text):
-        return ''.join(' ' + char if self.is_emoji(char) else char for char in text).strip()
-
-    # translator = Translator()
-    # import asyncio
-    # loop = asyncio.get_event_loop()
-
-    def translate_emojis(self, words):
-        word_list = list()
-        words_to_translate = list()
-        for word in words:
-            t = self.emojis_ar.get(word.get('emoji'), None)
-            if t is None:
-                word.update({'translation': 'عادي', 'translated': True})
-                # words_to_translate.append('normal')
-            else:
-                word.update({'translated': False, 'translation': t})
-                words_to_translate.append(t.replace(':', '').replace('_', ' '))
-            word_list.append(word)
-        return word_list
+    def translate_emojis(self, word):
+        return self.emojis_ar.get(word.group(), "None").replace(':','').replace('_',' ')
 
     def emoji_unicode_translation(self, text):
-        text = self.add_space(text)
-        words = text.split()
-        text_list = list()
-        emojis_list = list()
-        c = 0
-        for word in words:
-            if self.is_emoji(word):
-                emojis_list.append({'emoji': word, 'emplacement': c})
-            else:
-                text_list.append(word)
-            c += 1
-        emojis_translated = self.translate_emojis(emojis_list)
-        for em in emojis_translated:
-            text_list.insert(em.get('emplacement'), em.get('translation'))
-        text = " ".join(text_list)
+        text = self.emoji_pattern.sub(self.translate_emojis, text)
         return text
 
     def translate_emoji(self, text):
-        text = self.emoji_native_translation(text)
         text = self.emoji_unicode_translation(text)
         return text
+
     def remove_urls(self, text):
         text = re.sub('http\S+\s*', '', text)  # remove URLs
         return text
+
     def remove_number(self, text):
         text = re.sub("\d+", " ", text)
         return text
@@ -251,8 +176,8 @@ class Preprocessing:
     # Apply the selected filters
     # @st.cache
     def apply_filters_selected(self, bool_selected_filters):
-        if bool_selected_filters[0]: self.df[self.col_selected] = self.df[self.col_selected].apply(
-            lambda x: self.remove_row_with_English())
+        if bool_selected_filters[0]:
+            self.remove_row_with_English()
         if bool_selected_filters[1]: self.df[self.col_selected] = self.df[self.col_selected].apply(
             lambda x: self.remove_urls(x))
         if bool_selected_filters[2]: self.df[self.col_selected] = self.df[self.col_selected].apply(
@@ -271,5 +196,3 @@ class Preprocessing:
             lambda x: self.normalizeArabic(x))
         if bool_selected_filters[9]: self.df[self.col_selected] = self.df[self.col_selected].apply(
             lambda x: self.stem(x))
-
-        # df = df.sort_values(by='word_count', ascending=[0])
